525f9b5670333e9ae0b627c69a16e20988f07f53
kks <sosiluv@naver.com>
2021-03-11 09:08:19 +0900

    ip주소 수정해야함

--- app/src/main/java/com/fithe/login/common/Web.java
+++ app/src/main/java/com/fithe/login/common/Web.java
@@ -2,5 +2,6 @@ package com.fithe.login.common;
 
 public class Web {
     public static String ip = "192.168.0.4";
-    public static String servletURL = "http://" + ip + ":8082/fithe/"; //URL 공통 부분
+    public static String servletURL = "http://" + ip + ":8086/fithe/"; //URL 공통 부분
+    //local ip수정... localhost는 사용불가
 }
